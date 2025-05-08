00b8b39fa59b79b84eaf1ee1e59c7615d5712bc4
Sushant Shrestha <sushant.shrestha@foodmandu.biz>
2025-05-08 17:25:05 +0545

    uncommented

--- src/components/LayoutItems/LayoutItem.jsx
+++ src/components/LayoutItems/LayoutItem.jsx
@@ -1,42 +1,42 @@
-// import React, { useEffect, useState } from "react";
-// import { layoutitemlist } from "../Api";
-// import { useParams } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { layoutitemlist } from "../Api";
+import { useParams } from "react-router-dom";
 
-// const LayoutItemList = () => {
-//   const { layoutId } = useParams();
-//   const [layoutItems, setLayoutItems] = useState([]);
+const LayoutItemList = () => {
+  const { layoutId } = useParams();
+  const [layoutItems, setLayoutItems] = useState([]);
 
-//   const fetchLayoutItems = async () => {
-//     try {
-//       const data = await layoutitemlist();
-//       const filteredItems = data.filter(
-//         (item) => item.layoutId === parseInt(layoutId)
-//       );
-//       setLayoutItems(filteredItems);
-//     } catch (error) {
-//       console.error("Error fetching layout items:", error);
-//     }
-//   };
+  const fetchLayoutItems = async () => {
+    try {
+      const data = await layoutitemlist();
+      const filteredItems = data.filter(
+        (item) => item.layoutId === parseInt(layoutId)
+      );
+      setLayoutItems(filteredItems);
+    } catch (error) {
+      console.error("Error fetching layout items:", error);
+    }
+  };
 
-//   useEffect(() => {
-//     fetchLayoutItems();
-//   }, [layoutId]);
+  useEffect(() => {
+    fetchLayoutItems();
+  }, [layoutId]);
 
-//   return (
-//     <div className="grid items-center h-screen p-4 layout">
-//       <div className="grid grid-cols-4 gap-4">
-//         {layoutItems.map((item, index) => (
-//           <div key={index} className="grid-item">
-//             <img
-//               src={item.image}
-//               alt={`Image ${index}`}
-//               className="w-full h-full object-cover"
-//             />
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+  return (
+    <div className="grid items-center h-screen p-4 layout">
+      <div className="grid grid-cols-4 gap-4">
+        {layoutItems.map((item, index) => (
+          <div key={index} className="grid-item">
+            <img
+              src={item.image}
+              alt={`Image ${index}`}
+              className="w-full h-full object-cover"
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-// export default LayoutItemList;
+export default LayoutItemList;
